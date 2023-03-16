9b05eb30d76ef5b732ab8bdea29cf8dc81dd5347
sangyoon kim <sangyun0904@gmail.com>
2023-03-16 14:16:30 +0900

    fix dynamodb endpoint

--- user_server/plugins/dynamodb.js
+++ user_server/plugins/dynamodb.js
@@ -2,7 +2,7 @@ const fp = require('fastify-plugin')
  
 module.exports = fp(async function (fastify, opts) {
     fastify.register(require('fastify-dynamodb'), {
-        endpoint: 'http://dynamodb-local:8000',
+        endpoint: 'com.amazonaws.ap-northeast-2.dynamodb',
         region: "ap-northeast-2"
     })
 })
