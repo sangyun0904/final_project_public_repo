42a38e81f0905bb391ff5017b6e27c98a24046d8
jihoon <jihoooon555@gmail.com>
2023-03-17 21:41:42 +0900

    feat check by id

--- rewards-check/index.js
+++ rewards-check/index.js
@@ -6,26 +6,30 @@ const app = express();
 app.use(express.json());
 
 app.get('/rewards/:id', async (req, res, next) => {
-      const { id } = req. params;
+  const { id } = req.params;
 
-      const params = {
-        TableName: 'Rewards',
-        KeyConditionExpression: 'id = :id',
-      };
+  const params = {
+    TableName: 'Rewards',
+    KeyConditionExpression: 'id = :id',
+    ProjectionExpression: 'user_id, product_id',
+    ExpressionAttributeValues: {
+      ':id': Number(id)
+    }
+  };
 
-      try {
-        const data = await docClient.scan(params).promise();
-        res.status(200).json(data.Items);
-      } catch (err) {
-        console. error(err);
-        res.status(500).json({ error: 'Failed to query rewards' });
-      }
+  try {
+    const data = await docClient.query(params).promise();
+    res.status(200).json(data.Items);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to query rewards' });
+  }
 });
 
 app.use((req, res, next) => {
-      return res.status(404).json({
-        error: 'Not Found'
-      });
+  return res.status(404).json({
+    error: 'Not Found'
+  });
 });
 
 module.exports.handler = serverless(app);
