01ece1be556aa33d807be86b38d46c904283c364
sangyoon kim <sangyun0904@gmail.com>
2023-03-18 17:37:24 +0900

    Fix dynamodv endpoint, Add start --address 0.0.0.0

--- user_server/plugins/dynamodb.js
+++ user_server/plugins/dynamodb.js
@@ -2,7 +2,7 @@ const fp = require('fastify-plugin')
  
 module.exports = fp(async function (fastify, opts) {
     fastify.register(require('fastify-dynamodb'), {
-        endpoint: 'com.amazonaws.ap-northeast-2.dynamodb',
+        endpoint: 'vpce-068a9f16367c7e1b9',
         region: "ap-northeast-2"
     })
 })
