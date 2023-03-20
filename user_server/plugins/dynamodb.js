2b747613f52bf43679e7fea88b5dc1bc68eaab4a
sangyoon kim <sangyun0904@gmail.com>
2023-03-20 13:13:50 +0900

    Fix user attendacne method

--- user_server/plugins/dynamodb.js
+++ user_server/plugins/dynamodb.js
@@ -2,8 +2,8 @@ const fp = require('fastify-plugin')
  
 module.exports = fp(async function (fastify, opts) {
     fastify.register(require('fastify-dynamodb'), {
-        // endpoint: 'https://dynamodb.ap-northeast-2.amazonaws.com',
-        endpoint: 'http://localhost:8080',
+        endpoint: 'https://dynamodb.ap-northeast-2.amazonaws.com',
+        //endpoint: 'http://localhost:8080',
         region: "ap-northeast-2"
     })
 })
--- user_server/routes/attendance/index.js
+++ user_server/routes/attendance/index.js
@@ -1,16 +1,17 @@
 'use strict'
 
 var CUSTOMEPOCH = 1300000000000; // artificial epoch
-function generateRowId() {
-    var ts = new Date().getTime() - CUSTOMEPOCH; // limit to recent
+function generateRowId(shardId) {
+    const d = new Date()
+    var ts = d.getTime() - CUSTOMEPOCH; // limit to recent
     var randid = Math.floor(Math.random() * 512);
     ts = (ts * 64);   // bit-shift << 6
+    ts = ts + shardId;
     return (ts * 512) + randid;
 }
 
 module.exports = async function (fastify, opts) {
     fastify.get('/:user_id', async function (request, reply) {
-  
         let data
 
         // Set the parameters
@@ -31,18 +32,19 @@ module.exports = async function (fastify, opts) {
 
     fastify.post('/attend', async function (request, reply) {
         let data
+        let day = new Date()
 
         // Set the parameters
         const params = {
             TableName : 'Attendance',
             Item: {
-               id: generateRowId(),
-               date: Date().getTime(),
+               id: generateRowId(request.body.userId),
+               date: day.getTime(),
                user_id: request.body.userId
             }
         };
 
-        documentClient.put(params, function(err, data) {
+        await fastify.dynamo.put(params, function(err, data) {
             if (err) console.log(err);
         });
 
