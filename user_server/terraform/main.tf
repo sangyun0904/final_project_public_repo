d122dbad9410efbca5e9209bef21180b5636ae7d
sangyoon kim <sangyun0904@gmail.com>
2023-03-21 00:38:06 +0900

    Add terraform public route table

--- user_server/terraform/main.tf
+++ user_server/terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 0.12"
     required_providers {
         aws = {
-            source = "hashicorp/aws"
+            source  = "hashicorp/aws"
             version = "~> 4.0"
         }
     }
@@ -18,10 +18,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnets" {
-    count      = length(var.public_subnet_cidrs)
-    vpc_id     = aws_vpc.main.id
-    cidr_block = element(var.public_subnet_cidrs, count.index)
-    availability_zone = element(var.azs, count.index)
+    count               = length(var.public_subnet_cidrs)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = element(var.public_subnet_cidrs, count.index)
+    availability_zone   = element(var.azs, count.index)
  
     tags = {
         Name = "Public Subnet ${count.index + 1}"
@@ -29,10 +29,10 @@ resource "aws_subnet" "public_subnets" {
 }
  
 resource "aws_subnet" "private_subnets" {
-    count      = length(var.private_subnet_cidrs)
-    vpc_id     = aws_vpc.main.id
-    cidr_block = element(var.private_subnet_cidrs, count.index)
-    availability_zone = element(var.azs, count.index)
+    count               = length(var.private_subnet_cidrs)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = element(var.private_subnet_cidrs, count.index)
+    availability_zone   = element(var.azs, count.index)
  
     tags = {
         Name = "Private Subnet ${count.index + 1}"
@@ -45,4 +45,23 @@ resource "aws_internet_gateway" "gw" {
     tags = {
         Name = "Project VPC IG"
     }
+}
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+    
+    tags = {
+        Name = "Public subnets Route Table"
+    }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count          = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.public_rt.id
 }
